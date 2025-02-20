a1059ef4afb5fc8669ee567dbb7e59c87358dfc9
omkarbm03 <omkarbm03@gmail.com>
2025-02-20 02:26:19 -0500

    Added Solutions for Binary Search Problem

---
+++ Binary Search/BinarySearch.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            # (l + r) // 2 can lead to overflow
+            m = l + ((r - l) // 2)  
+
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+    
+
+#Example Usage
+solution = Solution()
+nums = [1,6,8,9,6,10,4]
+target = 10 
+solution.search(nums, target)
